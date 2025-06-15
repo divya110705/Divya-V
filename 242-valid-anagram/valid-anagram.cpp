2fda22d7cd643e475e49929d0b1cdbd8933ff689
Divya V <divyavijaykumar07@gmail.com>
2025-06-15 20:01:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.82%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+     if(s.size() != t.size())
+     return false;
+    int n = s.size();
+    int count[26] = {0};
+    for(int i=0; i<n; i++)
+    {
+        ++count[s[i]-97];
+        --count[t[i]-97];
+    } 
+    for(int i=0; i<26; i++)
+    if(count[i] != 0) return false;
+    return true; 
+    }
+};
